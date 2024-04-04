266916f16117e380015741cfee279d4c80621643
katarina.lum@hotmail.com <49263182+CodewKat@users.noreply.github.com>
2024-04-04 14:22:12 +1300

    Initial commit

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Calculator
+{
+    public class Calculator : CalculatorBase
+    {
+        public Calculator()
+        {
+        }
+
+
+        public override int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public override int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+
+        public override int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+
+        public override int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+
+
+}
+
---
+++ Calculator/Calculator/CalculatorBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Calculator
+{
+    public abstract class CalculatorBase
+    {
+
+        public abstract int Add(int num1, int num2);
+
+
+        public abstract int Subtract(int num1, int num2);
+
+
+
+        public abstract int Multiply(int num1, int num2);
+
+
+        public abstract int Divide(int num1, int num2);
+
+
+    }
+}
+
